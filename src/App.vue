144371fd5b02751b629f33f6cd4bd60942819008
Yekinni Basiru <nodejshobby@gmail.com>
2022-09-01 13:15:37 +0100

    My redesign of my portfolio site

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: Roboto;
+    line-height: 1.7;
+  }
+</style>
